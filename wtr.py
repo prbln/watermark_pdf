536c2b955eaa43303ba94e2be7f55b6d76d1f138
prbln <prabhleensaini14@gmail.com>
2020-06-29 21:11:17 +0530

    pdf to be water marked

---
+++ wtr.py
@@ -0,0 +1,16 @@
+import PyPDF2
+import sys 
+inputs = sys.argv[1:]
+#opening and accessing the wtr-mark pg
+reader1 = PyPDF2.PdfFileReader( open('watermark.pdf','rb') )
+
+#accessing each input pdf one by one
+output = PyPDF2.PdfFileWriter()
+for pdf in inputs:
+	reader = PyPDF2.PdfFileReader(open(pdf,'rb') )
+	for page_no in range(reader.numPages):
+		page = reader.getPage(page_no)
+		page.mergePage(reader1.getPage(0))
+		output.addPage(page)
+		with open('water_super.pdf','wb') as new_file:
+			output.write(new_file)
